16d2b3e35baa55a04c7dd4e6b363e70eda011d7d
Karlo Pagtakhan <kidap@yahoo.com>
2016-01-04 15:47:27 -0500

    Working code

--- FuzzBizzNezz/main.c
+++ FuzzBizzNezz/main.c
@@ -9,7 +9,33 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int Fuzz, Bizz, Nezz;
+    int ctr = 1;
+    
+    // Get 3 integers
+    printf("Enter a number#1 (Fuzz)");
+    scanf("%d", &Fuzz);
+    printf("Enter a number#2 (Bizz)");
+    scanf("%d", &Bizz);
+    printf("Enter a number#3 (Nezz)");
+    scanf("%d", &Nezz);
+    
+    
+    // Display numbers
+    do{
+        if (ctr % Fuzz == 0 && ctr % Bizz == 0 ){
+            printf("FuzzBizz \n");
+        } else if (ctr % Fuzz == 0){
+            printf("Fuzz \n");
+        }else if (ctr % Bizz == 0){
+            printf("Bizz \n");
+        }
+        else{
+            printf("%d \n", ctr);
+        }
+        ctr++;
+        
+    }while(ctr <= Nezz);
+    
     return 0;
 }
